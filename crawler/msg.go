e3f741d7b2c5c9bd64054ba9af2ea116d8cb1a15
mikelsr <mikelsr@pm.me>
2023-09-23 18:57:33 +0200

    Create a message structure for url events

--- crawler/msg.go
+++ crawler/msg.go
@@ -10,26 +10,29 @@ import (
 type MessageType int
 
 const (
-	Report MessageType = iota
-	Claim
+	Visit  MessageType = iota // Visited URL
+	Claim                     // Found and claimed URL
+	Report                    // Found URL but did not claim it
 )
 
 type Message struct {
 	MessageType `json:"type"`
-	Origin      string   `json:"origin"`
-	Claimed     []string `json:"claimed"`
-	Unclaimed   []string `json:"unclaimed"`
+	Urls        []string `json:"urls"`
 }
 
-func MakeItem(raftId uint64, msg Message) (raft.Item, error) {
+func (m Message) AsItem(raftId uint64) (raft.Item, error) {
 	b := make([]byte, 8)
 	binary.LittleEndian.PutUint64(b, raftId)
-	m, err := json.Marshal(msg)
+	mb, err := json.Marshal(m)
 	if err != nil {
 		return raft.Item{}, err
 	}
 	return raft.Item{
 		Key:   b,
-		Value: m,
+		Value: mb,
 	}, nil
+}
+
+func (m *Message) FromItem(item raft.Item) error {
+	return json.Unmarshal(item.Value, m)
 }
