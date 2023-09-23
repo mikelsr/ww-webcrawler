6c975cf09175d513a0da995646a6387b188c4f61
mikelsr <mikelsr@pm.me>
2023-09-23 18:57:23 +0200

    Keep track of set size

--- crawler/set.go
+++ crawler/set.go
@@ -3,31 +3,57 @@ package main
 import (
 	"math/rand"
 	"sync"
+	"sync/atomic"
 )
 
 type Set[T any] struct {
-	size int
-	*sync.Map
+	size *atomic.Int32
+	sm   *sync.Map
+}
+
+func NewSet[T any]() Set[T] {
+	return Set[T]{
+		size: &atomic.Int32{},
+		sm:   &sync.Map{},
+	}
 }
 
 func (s Set[T]) Put(t T) {
-	s.Store(t, true)
+	_, ok := s.sm.Load(t)
+	if !ok {
+		s.size.Add(1)
+	}
+	s.sm.Store(t, true)
+}
+
+func (s Set[T]) Has(t T) bool {
+	_, ok := s.sm.Load(t)
+	return ok
 }
 
 func (s Set[T]) Pop(k T) (T, bool) {
-	v, ok := s.LoadAndDelete(k)
-	if !ok {
-		return k, ok
+	_, ok := s.sm.LoadAndDelete(k)
+	if ok {
+		s.size.Add(-1)
 	}
-	return v.(T), ok
+	return k, ok
 }
 
 func (s Set[T]) PopRandom() (T, bool) {
 	var k *T
-	s.Map.Range(func(key, value any) bool {
-		*k = value.(T)
+	size := s.size.Load()
+	s.sm.Range(func(key, value any) bool {
+		*k = key.(T)
 		// Each item has a 1/size chance of stopping the iteration
-		return s.size%rand.Int() != 0
+		return size%rand.Int31() != 0
 	})
 	return s.Pop(*k)
+}
+
+func (s Set[T]) Range(f func(key, value any) bool) {
+	s.sm.Range(f)
+}
+
+func (s Set[T]) Size() int {
+	return int(s.size.Load())
 }
